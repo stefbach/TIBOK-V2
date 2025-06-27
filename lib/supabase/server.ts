6da9af3de61f4e99af9abbc1196f4ddc56feb131
stefbach <sbach1964@gmail.com>
2025-06-27 03:57:19 +0200

    Add Supabase env and use server variables

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -5,7 +5,7 @@ export function createClient() {
   const cookieStore = cookies()
 
   // Create a supabase client on the server with project's credentials
-  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+  return createServerClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!, {
     cookies: {
       get(name: string) {
         return cookieStore.get(name)?.value
